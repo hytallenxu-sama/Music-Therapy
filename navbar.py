b88a94023e400bf3ef68505d514d2775bfce7057
hytallenxu-sama <155510739+hytallenxu-sama@users.noreply.github.com>
2024-08-11 19:07:55 +0800

    Update: Upload Navigation Bar Module

---
+++ navbar.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+navi_dic={
+    'Home':'/home',
+    'Discover':'/discover',
+    'Settings':'/settings'
+}
+
+def navbar(page, s:int):
+    def on_nav_change(e: ft.ControlEvent):
+        selected_index = e.control.selected_index
+        selected_item = nav_bar.destinations[selected_index].label
+        #print(f"Selected item: {selected_item}")
+        page.go(navi_dic[selected_item])
+
+    nav_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(label="Home", icon=ft.icons.HOME),
+            ft.NavigationBarDestination(label="Discover", icon=ft.icons.PERSON),
+            ft.NavigationBarDestination(label="Settings", icon=ft.icons.SETTINGS)
+        ],
+        selected_index=s,  # Default selected item
+        on_change=on_nav_change  # Event handler for changes
+    )
+    return nav_bar
